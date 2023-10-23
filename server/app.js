9716bd737a8b46a4477579bbc7a2e3c0f5b7f0bc
Erdabi <yusuf.khurshid@gmx.ch>
2023-10-23 10:51:07 +0200

    initial commit

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { query } = require('express-validator');
+const http = require("http");
+const { initializeAPI } = require("./api");
+
+// Create the express server
+const app = express();
+app.use(express.json());
+app.get('/hello', query('person').notEmpty(), (req, res) => {
+  res.send(`Hello, ${req.query.person}!`);
+});
+const server = http.createServer(app);
+
+// deliver static files from the client folder like css, js, images
+app.use(express.static("client"));
+// route for the homepage
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/client/index.html");
+});
+
+// Initialize the REST api
+initializeAPI(app);
+
+//start the web server
+const serverPort = 3000;
+server.listen(serverPort, () => {
+  console.log(`Express Server started on port ${serverPort}`);
+});
